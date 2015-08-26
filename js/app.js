dfe711826e4ebf650373ee757d978518be88062a
mrmyers5280 <not.that.mike.myers@gmail.com>
2015-08-26 14:15:50 -0600

    With app.js, add code to make AJAX call to retrieve images from Instagram API

--- js/app.js
+++ js/app.js
@@ -1,4 +1,25 @@
 // app.js
 $(function() {
 	$( "#datepicker" ).datepicker();
+	// var endpoint = 'https://api.instagram.com/v1/tags/' + tag + '/media/recent?client_id=6734a9a21d4c47a39050e15a0487adc8';
+	var endpoint = 'https://api.instagram.com/v1/tags/gigharbor/media/recent?client_id=6734a9a21d4c47a39050e15a0487adc8';
+	$.ajax({
+		url: endpoint,
+		dataType: "jsonp",
+		cache: "false",
+		type: "GET"
+	})
+	.done(function(response) {
+		console.log(response);
+	})
+	.fail(function(jqXHR, error, errorThrown){
+		var errorElem = showError(error);
+		$('.search-results').append(errorElem);
+	});
+	// takes error string and turns it into displayable DOM element
+	var showError = function(error){
+		var errorElem = $('.templates .error').clone();
+		var errorText = '<p>' + error + '</p>';
+		errorElem.append(errorText);
+	};
   });
