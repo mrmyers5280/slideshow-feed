382c17b5e87e118b48a8f92b5c3ccede0f77fcd8
mrmyers5280 <not.that.mike.myers@gmail.com>
2015-08-26 14:33:25 -0600

    With app.js, move functions out of doc-ready function and into getImages function

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
 // app.js
 $(function() {
 	$( "#datepicker" ).datepicker();
-	// var endpoint = 'https://api.instagram.com/v1/tags/' + tag + '/media/recent?client_id=6734a9a21d4c47a39050e15a0487adc8';
-	var endpoint = 'https://api.instagram.com/v1/tags/gigharbor/media/recent?client_id=6734a9a21d4c47a39050e15a0487adc8';
+	getImages('gigharbor');
+});
+
+var showImages = function(igObject) {
+
+};
+
+var getImages = function(tag) {
+	var clientID = '6734a9a21d4c47a39050e15a0487adc8'
+	var endpoint = 'https://api.instagram.com/v1/tags/' + tag + '/media/recent?client_id=' + clientID;
+	// var endpoint = 'https://api.instagram.com/v1/tags/gigharbor/media/recent?client_id=6734a9a21d4c47a39050e15a0487adc8';
 	$.ajax({
 		url: endpoint,
 		dataType: "jsonp",
@@ -11,15 +20,16 @@ $(function() {
 	})
 	.done(function(response) {
 		console.log(response);
+		showImages(response);
 	})
 	.fail(function(jqXHR, error, errorThrown){
 		var errorElem = showError(error);
 		$('.search-results').append(errorElem);
 	});
-	// takes error string and turns it into displayable DOM element
-	var showError = function(error){
-		var errorElem = $('.templates .error').clone();
-		var errorText = '<p>' + error + '</p>';
-		errorElem.append(errorText);
-	};
-  });
+};
+// takes error string and turns it into displayable DOM element
+var showError = function(error){
+	var errorElem = $('.templates .error').clone();
+	var errorText = '<p>' + error + '</p>';
+	errorElem.append(errorText);
+};
